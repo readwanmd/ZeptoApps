6604ecd0592b60faa16b37c6ae6d9b418d63b80a
readwanmd <readwanmd@gmail.com>
2024-10-18 21:38:58 +0600

    toast added: api takes time to response also title and favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AppRoutes from './routes/AppRoutes';
 
 const App = () => {
+	useEffect(() => {
+		toast.dark('API takes time to give response. Please Consider it.', {
+			autoClose: 5000,
+		});
+	}, []);
+
 	return (
 		<>
 			<BrowserRouter>
